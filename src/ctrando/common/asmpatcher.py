63934d82f46c055c74a9ff6d3bbe9e8b6a7a63ff
pseudoarc <indecomposable@gmail.com>
2026-07-07 13:20:25 -0400

    Add ability to force bank for some patches.

--- src/ctrando/common/asmpatcher.py
+++ src/ctrando/common/asmpatcher.py
@@ -36,7 +36,8 @@ def apply_jmp_patch(
 def add_jsl_routine(
         routine: assemble.ASMList,
         ct_rom: ctrom.CTRom,
-        hint: int = 0
+        hint: int = 0,
+        force_bank: bool = False
 ) -> int:
     """
     Adds a subroutine which should be called with JSL.
@@ -45,7 +46,35 @@ def add_jsl_routine(
 
     routine_b = assemble.assemble(routine)
     routine_addr = ct_rom.space_manager.get_free_addr(len(routine_b), hint)
+
+    if force_bank and (routine_addr >> 16) != (hint >> 16):
+        raise ctrom.freespace.FreeSpaceError(f"Not enough space in bank {hint >> 16:02X} (need {len(rt_b)})")
+
+
     ct_rom.seek(routine_addr)
     ct_rom.write(routine_b, freespace.FSWriteType.MARK_USED)
 
+
     return routine_addr
+
+
+def apply_jsr_patch(
+        jsr_routine: assemble.ASMList,
+        ct_rom: ctrom.CTRom,
+        hook_addr: int,
+) -> int:
+    """Insert a JSR to a new routine at hook_addr.  Returns routines addr."""
+
+    rt_b = assemble.assemble(jsr_routine)
+    rt_addr = ct_rom.space_manager.get_free_addr(
+        len(rt_b), hook_addr & 0xFF0000
+    )
+    if rt_addr >> 16 != hook_addr >> 16:
+        raise ctrom.freespace.FreeSpaceError(f"Not enough space in bank {hook_addr >> 16:02X} (need {len(rt_b)})")
+
+    hook_b = inst.JSR(rt_addr & 0xFFFF, AM.ABS).to_bytearray()
+    ct_rom.seek(rt_addr)
+    ct_rom.write(rt_b, ctrom.freespace.FSWriteType.MARK_USED)
+
+    ct_rom.seek(hook_addr)
+    ct_rom.write(hook_b)
